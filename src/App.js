c17e7d52db1a1d4f9143e15d9d70022b304d522f
sauravgupta2800 <sauravgupta2800@gmail.com>
2021-05-08 20:05:44 +0530

    1.9: unicafe step4

--- src/App.js
+++ src/App.js
@@ -8,12 +8,18 @@ const Statistics = ({ good, neutral, bad, all, average, positive }) => {
   return (
     <Fragment>
       <h1>Statistics</h1>
-      <div>good {good}</div>
-      <div>neutral {neutral}</div>
-      <div>bad {bad}</div>
-      <div>all {all}</div>
-      <div>average {average}</div>
-      <div>positive {positive}</div>
+      {!all ? (
+        <div>No feeback given</div>
+      ) : (
+        <div>
+          <div>good {good}</div>
+          <div>neutral {neutral}</div>
+          <div>bad {bad}</div>
+          <div>all {all}</div>
+          <div>average {average}</div>
+          <div>positive {positive}</div>
+        </div>
+      )}
     </Fragment>
   );
 };
